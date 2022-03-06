7afe30ab8bd63761ea35f3d9efeea4d9bd7ca46c
kszajko46 <khszajko46@gmail.com>
2022-03-06 12:50:24 -0500

    added form and border

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,20 @@
 export default function Search() {
   return (
-    <div>
-      <form id="search-form" class="mb-4">
-        <div class="row">
-          <div class="col-8">
-            <input
-              type="search"
-              placeholder="Enter a City"
-              class="form-control"
-              id="city-search"
-              autocomplete="off"
-            />
-          </div>
-          <div class="col-2">
-            <input type="submit" value="search" class="btn btn-primary" />
-          </div>
+    <form id="search-form" class="mb-4">
+      <div class="row">
+        <div class="col-8">
+          <input
+            type="search"
+            placeholder="Enter a City"
+            class="form-control"
+            id="city-search"
+            autocomplete="off"
+          />
         </div>
-      </form>
-    </div>
+        <div class="col-2">
+          <input type="submit" value="search" class="btn btn-primary" />
+        </div>
+      </div>
+    </form>
   );
 }
