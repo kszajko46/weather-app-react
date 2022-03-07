8b837da5c2dbd32a06219f4a6e38b781223be974
kszajko46 <khszajko46@gmail.com>
2022-03-07 09:09:10 -0500

    updated link to linkedin and github removed unwanted code and files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React from "react";
+
 import "./App.css";
 import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
-    <div class="container">
-      <div class="weather-app-wrapper">
-        <div class="weather-app">
-          <h3>weather search app</h3>
-          <Search />
+    <div className="App">
+      <div class="container">
+        <div class="weather-app-wrapper">
+          <div class="weather-app">
+            <Search />
+          </div>
         </div>
       </div>
+      <small>
+        <a href="https://github.com/kszajko46" target="_blank">
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a href="https://www.linkedin.com/in/kata-szajko/" target="_blank">
+          Kata Szajko
+        </a>
+      </small>
     </div>
   );
 }
-
-export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 export default function Search() {
-  return (
-    <form id="search-form" class="mb-4">
-      <div class="row">
-        <div class="col-8">
-          <input
-            type="search"
-            placeholder="Enter a City"
-            class="form-control"
-            id="city-search"
-            autocomplete="off"
-          />
-        </div>
-        <div class="col-2">
-          <input type="submit" value="search" class="btn btn-primary" />
-        </div>
-      </div>
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temp: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1921aa558d6b3e6d5b52f0eeb4ab5751&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function currentCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        autocomplete="off"
+        autofocus="on"
+        placeholder="Enter a city"
+        id="search-city"
+        onChange={currentCity}
+      />
+      <input type="submit" value="search" />
     </form>
   );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temp)}℃</li>
+          <li>Wind: {Math.round(weather.wind)}km</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>description: {weather.description}</li>
+          <li>
+            <img src={weather.icon} alt="Weather Icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
